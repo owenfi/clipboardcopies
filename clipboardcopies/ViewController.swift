d157854c68b1757e047c4e6281d2b8c8470009eb
Owen Imholte <git@owenimholte.com>
2021-11-09 22:05:12 -0800

    Initial Commit

---
+++ clipboardcopies/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  clipboardcopies
+//
+//  Created by Owen Imholte on 11/9/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
